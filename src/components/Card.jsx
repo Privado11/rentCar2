89c462cc69ea0bdc5310e78d8e7ef5488850e5ac
Privado11 <wjimenez@unimagdalena.edu.co>
2024-06-11 15:11:31 -0500

    Feat: Agregada opción de borrar filtros

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,35 @@
-import React, { useContext } from 'react'
-import '../styles/CardStyle.css'
-import { Link } from 'react-router-dom';
-import { contexto } from '../context/ContextoGeneral';
-import imgAuto from '../assets/model-3.avif'
+import React, { useContext } from "react";
+import "../styles/CardStyle.css";
+import { Link } from "react-router-dom";
+import { contexto } from "../context/ContextoGeneral";
+import imgAuto from "../assets/model-3.avif";
 
-function Card({auto}) {
-    const {setAutoRentar} = useContext(contexto);
-    const {model,year,price,city,id} = auto;
+function Card({ auto }) {
+  const { setAutoRentar } = useContext(contexto);
+  const { brand, model, year, price, city, id } = auto;
 
-    function handleClick(){
-      setAutoRentar(auto);
-    }
-    
+  function handleClick() {
+    setAutoRentar(auto);
+  }
+
   return (
-    <div className='card'>
-        <div className='contenedorImagen'>
-            <img src={imgAuto} alt="imagen de auto" />
-        </div>
-        <h2>{model}</h2>
-        <p>{year}</p>
-        <span>{city.name}</span>
-        <span key={1}>${price}</span>
-        <button onClick={handleClick}>
-          <Link to={'registro'} className='linkCard'>
-            Rentar
-          </Link>
-        </button>
+    <div className="card">
+      <div className="contenedorImagen">
+        <img src={imgAuto} alt="imagen de auto" />
+      </div>
+      <h2>{brand}</h2>
+      <p>
+        {model} {year}
+      </p>
+      <span>{city.name}</span>
+      <span key={1}>${price}</span>
+      <button onClick={handleClick}>
+        <Link to={"registro"} className="linkCard">
+          Rentar
+        </Link>
+      </button>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/views/Renta.jsx
+++ src/views/Renta.jsx
@@ -1,63 +1,45 @@
-import React, { useContext } from 'react'
-import '../styles/RentaStyle.css'
-import { contexto } from '../context/ContextoGeneral'
+import React, { useContext } from "react";
+import "../styles/RentaStyle.css";
+import { contexto } from "../context/ContextoGeneral";
 
 function Renta() {
-    const {rentaActual,filtroActual} = useContext(contexto);
-    function modificarFecha(fecha){
-        const newFecha = fecha.split('T')
-        return newFecha[0]
-    }
+  const { rentaActual, filtroActual } = useContext(contexto);
+  function modificarFecha(fecha) {
+    const newFecha = fecha.split("T");
+    return newFecha[0];
+  }
   return (
-    <div className='containerView'>
-        <div className='containerRenta'>
-            <h2>Auto Rentado</h2>
-            <h3>Propietario</h3>
-            <label>Nombre:</label>
-            <p>
-                {rentaActual.name+' '+rentaActual.lastName}
-            </p>   
-            <label>Dirección:</label>
-            <p>
-                {rentaActual.address}
-            </p>  
-            <label>Telefono:</label>
-            <p>
-                {rentaActual.phone}
-            </p> 
-            <label>Ciudad:</label>
-            <p>
-                {rentaActual.city.name}
-            </p> 
-            <h3>Auto</h3>
-            <label>Modelo:</label>
-            <p>
-                {rentaActual.model}
-            </p>   
-            <label>Precio:</label>
-            <p>
-                {rentaActual.price}
-            </p>  
-            <label>Año:</label>
-            <p>
-                {rentaActual.year}
-            </p>
-            <label>Placa:</label>
-            <p>
-                {rentaActual.licensePlate}
-            </p>     
-            <h3>Fecha de Renta</h3>
-            <label>Fecha Inicial:</label>
-            <p>
-                {modificarFecha(filtroActual.fechaI)}
-            </p>
-            <label>Fecha Final:</label>
-            <p>
-                {modificarFecha(filtroActual.fechaF)}
-            </p>
-        </div>
+    <div className="containerView">
+      <div className="containerRenta">
+        <h2>Auto Rentado</h2>
+        <h3>Propietario</h3>
+        <label>Nombre:</label>
+        <p>{rentaActual.name + " " + rentaActual.lastName}</p>
+        <label>Dirección:</label>
+        <p>{rentaActual.address}</p>
+        <label>Telefono:</label>
+        <p>{rentaActual.phone}</p>
+        <label>Ciudad:</label>
+        <p>{rentaActual.city.name}</p>
+        <h3>Auto</h3>
+        <label>Marca:</label>
+        <p>{rentaActual.brand}</p>
+        <label>Modelo:</label>
+        <p>{rentaActual.model}</p>
+        <label>Precio:</label>
+        <p>{rentaActual.price}</p>
+        <label>Año:</label>
+        <p>{rentaActual.year}</p>
+        <label>Placa:</label>
+        <p>{rentaActual.licensePlate}</p>
+        <h3>Fecha de Renta</h3>
+        <label>Fecha Inicial:</label>
+        <p>{modificarFecha(filtroActual.fechaI)}</p>
+        <label>Fecha Final:</label>
+        <p>{modificarFecha(filtroActual.fechaF)}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Renta
+export default Renta;
